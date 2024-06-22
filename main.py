577acfcf96688c028dfc38eeae6fa12e21cb810b
Bruce Brown <bbrown32@student.cvtc.edu>
2024-06-22 17:54:04 -0500

    Add datasets and imports

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # This is a Python script for predicting loan eligibility
 
+import numpy as np
+import pandas as pd
+
+
+# Store the datasets as Data Frame variables
+trainDf = pd.read_csv('loan-train.csv')
+testDF = pd.read_csv('loan-test.csv')
+
+# View the first few rows of the Data Frame datasets
+# print(trainDf.head())
+# print(testDF.head())
 
